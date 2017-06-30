5e38a90de15975a91ca10f648abb5fc3a194199b
Hyeongjin Kim <hyeongjin184@gmail.com>
2017-06-30 15:30:48 +0900

    어렵다

--- django_app/config/settings/debug.py
+++ django_app/config/settings/debug.py
@@ -1,6 +1,11 @@
+# debug.py
 from .base import *
 
 config_secret_debug = json.loads(open(CONFIG_SECRET_DEBUG_FILE).read())
 
+# 디버그모드니까 DEBUG는 True
 DEBUG = True
 ALLOWED_HOSTS = config_secret_debug['django']['allowed_hosts']
+
+print('@@@@@@ DEBUG:', DEBUG)
+print('@@@@@@ ALLOWED_HOSTS:', ALLOWED_HOSTS)
